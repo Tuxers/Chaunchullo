0ed0aade6fb19bdaf704553debfcb5b4a76a5cf9
donkeysharp <serguimant@gmail.com>
2015-03-13 12:04:31 -0400

    Simplified the views.py rendering

--- board/views.py
+++ board/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 from board.models import *
 
 def index(request):
     posts = Post.objects.order_by('-created_at')
-
-    template = loader.get_template('posts/index.html')
-    context = RequestContext(request, {
-        'posts': posts
+    return render(request, 'posts/index.html', {
+        'posts' : posts
     })
-    return HttpResponse(template.render(context))
 
 def get_post(request, post_id):
     return HttpResponse('a single post' + post_id)
