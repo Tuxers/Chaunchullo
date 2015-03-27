1d2c9cc0a9c6575b434ac12d78905e74d2388c91
vengadora <vengadoravg@gmail.com>
2015-03-27 14:18:41 -0400

    vector of registered tags

--- board/views.py
+++ board/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from board.models import *
+import re
+import json
 
 def index(request):
     posts = Post.objects.order_by('-created_at')
@@ -23,7 +25,15 @@ def make_post(request):
     title = request.POST['title']
     content = request.POST['content']
 
+
     new_post = Post(title=title, content=content)
+    found_tags = re.findall('#\S*(?=\s)|#\S*(?=$)', content);
+    registered_tags = []
+
+    for tag in found_tags:
+        if len(Tag.objects.all().filter(name=tag)) != 0:
+            registered_tags.append(tag)
+
     new_post.save()
 
-    return HttpResponse(title + ' - ' + content)
+    return HttpResponse(json.dumps(registered_tags), content_type="application/json")
